510d7e17f01403e23ea653b3a2f0c1cf4e795129
nicolasp <nicolas.petit@keble.ox.ac.uk>
2024-01-25 16:15:33 +0000

    fix run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 python3 -m venv /tmp/lobster_tools_venv
 source /tmp/lobster_tools_venv/bin/activate
 pip3 install --upgrade pip setuptools wheel
-pip3 install lobster_tools/
+pip3 install .
 python3 -m tests.test_sim
