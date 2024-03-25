11218788796af59a2210840dc704c32d21899641
nicolasp <nicolas.petit@keble.ox.ac.uk>
2024-03-25 16:11:14 +0000

    wip for mid to mid

--- lobster_tools/returns.py
+++ lobster_tools/returns.py
@@ -1,10 +1,13 @@
+import datetime as dt
 from decimal import Decimal
 
 import numpy as np
 import pandas as pd
-from absl import app, flags
+from absl import app, flags, logging
+from arcticdb import Arctic
 
 from lobster_tools import config  # noqa: F401
+from lobster_tools.equity_features import aggregate_duplicates
 from lobster_tools.utils import get_dir
 
 FLAGS = flags.FLAGS
@@ -14,7 +17,7 @@ MARKOUTS = [Decimal(x) for x in ["0.125", "0.25", "0.5", "1.0", "2.0", "4.0"]]
 
 
 def get_log_returns(group, resample_freq: str, markouts: list[Decimal]):
-    """Compute resampled log returns. Markouts computed from base and markout series. 
+    """Compute resampled log returns. Markouts computed from base and markout series.
     Base timeseries using last value in ( ] and stamped right. Marked out price is first
     value of shifted ( ] stamped left."""
     base_offset = pd.Timedelta("0S")
@@ -43,8 +46,63 @@ def get_log_returns(group, resample_freq: str, markouts: list[Decimal]):
     return df
 
 
+_BBO_COLUMNS = [
+    "ask_price_1",
+    "ask_size_1",
+    "bid_price_1",
+    "bid_size_1",
+]
+
+
 def main(_):
-    # TODO: load from arctic and use mid to mid
+    # TODO: think if actually move code back to read_from_s3.py? or if just should parse
+    # flags separately
+
+    # parse flags
+    date_range = (
+        tuple(
+            dt.datetime.strptime(date, "%Y-%m-%d").date() for date in FLAGS.date_range
+        )
+        if FLAGS.date_range
+        else None
+    )
+
+    arctic = Arctic(FLAGS.s3_uri)
+    arctic_library = arctic[FLAGS.library]
+
+    # fetch data
+    logging.info(f"Fetching data for ticker={FLAGS.etf}")
+
+    etf_bbo = arctic_library.read(
+        symbol=FLAGS.etf,
+        columns=_BBO_COLUMNS,
+        date_range=date_range,
+    ).data
+
+    # TODO: UP TO HERE ON SUNDAY 24 march
+    # calculate what frequency to resample at
+    finest_resample_freq = min(FLAGS.resample_freqs, key=pd.Timedelta)
+    min_decimal_markout = min(MARKOUTS)
+    bbo_resample_freq = finest_resample_freq * min_decimal_markout
+
+
+    etf_bbo = etf_bbo.groupby(etf_bbo.index.date, observed=False).resample(
+        bbo_resample_freq, label="right", closed="right"
+    ).last()
+
+    print('hi')
+
+    etf_bbo = etf_bbo.resample(
+        finest_resample_freq, label="right", closed="right"
+    ).last()
+
+    etf_bbo = etf_bbo.eval("mid = (bid_price_1 + ask_price_1) / 2").eval(
+        "weighted_mid = (ask_size_1 * bid_price_1 + bid_size_1 * ask_price_1) / (ask_size_1 + bid_size_1)"
+    )
+
+    print("hi there")
+
+    # TODO: load from arctic and use mid to mid (since cancellations can move mid)
     dir_ = get_dir(etf=FLAGS.etf)
     etf_executions = pd.read_pickle(dir_ / "etf_executions.pkl")
 
